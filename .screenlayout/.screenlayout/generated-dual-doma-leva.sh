f5f0d71a8de76e6d8fe1ade32d062a5ee73bf7b0
Tomas Dedic <dedtom@gmail.com>
2023-10-04 15:00:53 +0200

    codeium

--- .screenlayout/.screenlayout/generated-dual-doma-leva.sh
+++ .screenlayout/.screenlayout/generated-dual-doma-leva.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output eDP-1 --mode 1920x1080 --pos 2560x240 --rotate normal --output DP-2 --off
+xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-2-8 --off --output DP-2-1 --off
