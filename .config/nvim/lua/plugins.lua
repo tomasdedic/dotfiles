22b491a6d7c11b151e72972c9d4ed5e8d060917d
Tomas Dedic <dedtom@gmail.com>
2023-10-06 11:59:16 +0200

    codeium

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -165,20 +165,15 @@ return require("packer").startup {
 
    use {
      -- also exists codeium.nvim with vim-cmp integration instead of virtual text
-     "Exafunction/codeium.vim",
+     "Exafunction/codeium.nvim",
       requires = {
         "nvim-lua/plenary.nvim",
         "hrsh7th/nvim-cmp",
       },
-     -- stylua: ignore
-     config = function ()
-       vim.g.codeium_disable_bindings = 1
-       vim.keymap.set("i", "<A-d>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
-       vim.keymap.set("i", "<A-f>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
-       vim.keymap.set("i", "<A-b>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
-       vim.keymap.set("i", "<A-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
-       vim.keymap.set("i", "<A-s>", function() return vim.fn["codeium#Complete"]() end, { expr = true })
-     end,
+      config = function()
+        require("codeium").setup({
+        })
+      end
    }
 
     -- use {
