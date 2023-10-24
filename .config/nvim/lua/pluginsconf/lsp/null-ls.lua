afe14069005c83637e4752af5dd2d6b612a2aac8
Tomas Dedic <dedtom@gmail.com>
2023-10-24 17:27:35 +0200

    golang lsp

--- .config/nvim/lua/pluginsconf/lsp/null-ls.lua
+++ .config/nvim/lua/pluginsconf/lsp/null-ls.lua
@@ -1,9 +1,33 @@
 local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-local opts = {
+-- local opts = {
+--   sources = {
+--     null_ls.builtins.formatting.gofmt,
+--     null_ls.builtins.formatting.goimports_reviser,
+--     null_ls.builtins.formatting.golines,
+--   },
+--   on_attach = function(client, bufnr)
+--     if client.supports_method("textDocument/formatting") then
+--       vim.api.nvim_clear_autocmds({
+--         group = augroup,
+--         buffer = bufnr,
+--       })
+--       vim.api.nvim_create_autocmd("BufWritePre", {
+--         group = augroup,
+--         buffer = bufnr,
+--         callback = function()
+--           vim.lsp.buf.format({ bufnr = bufnr })
+--         end,
+--       })
+--     end
+--   end,
+-- }
+-- return opts
+
+null_ls.setup {
   sources = {
-    null_ls.builtins.formatting.gofumpt,
+    null_ls.builtins.formatting.gofmt,
     null_ls.builtins.formatting.goimports_reviser,
     null_ls.builtins.formatting.golines,
   },
@@ -22,9 +46,8 @@ local opts = {
       })
     end
   end,
-}
-return opts
 
+}
 
 --local null_ls = require "null-ls"
 --local b = null_ls.builtins
