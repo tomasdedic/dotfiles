6e096877133698876c561c9c066e49b75d8f518c
Tomas Dedic <dedtom@gmail.com>
2023-11-03 11:55:27 +0100

    nvim toggleterm

--- .config/nvim/lua/pluginsconf/toggleterm.lua
+++ .config/nvim/lua/pluginsconf/toggleterm.lua
@@ -1,14 +1,27 @@
 require("toggleterm").setup {
   open_mapping = [[<C-\>]],
   autochdir = true,
-  direction = 'float',
+  direction = 'horizontal',
   float_opts = {
     border = 'single',
     width = 100,
   },
+  hide_numbers = true,
+  shade_terminals = false,
+  highlights = {
+    -- highlights which map to a highlight group name and a table of it's values
+    -- NOTE: this is only a subset of values, any group placed here will be set for the terminal window split
+    Normal = {
+      guibg = "White",
+    },
+  },
+  winbar = {
+    enabled = true,
+    name_formater= "term"
+  },
   size = function(term)
     if term.direction == "horizontal" then
-      return 25
+      return 15
     elseif term.direction == "vertical" then
       return 80
     end
