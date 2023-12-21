ff2ca23a7df1c650969191e205faa38dba765adf
Tomas Dedic <dedtom@gmail.com>
2023-12-21 17:31:26 +0100

    loki config changes

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -346,6 +346,15 @@ require("lazy").setup({
       require "pluginsconf.tokyonight"
     end,
   },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   lazy = false, -- make sure we load this during startup
+  --   priority = 1000,
+  --   config = function()
+  --     require "pluginsconf.catppuccin"
+  --   end,
+  -- },
 
   {
       "NLKNguyen/papercolor-theme",
