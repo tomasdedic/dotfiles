3210c75cb546d1a641829442e385f48f86714c7e
Tomas Dedic <tdedic@healthequity.com>
2024-04-26 08:14:01 +0200

    alacritty

--- .config/nvim/lua/pluginsconf/harpoon.lua
+++ .config/nvim/lua/pluginsconf/harpoon.lua
@@ -14,7 +14,7 @@ local function toggle_telescope(harpoon_files)
         finder = require("telescope.finders").new_table({
             results = file_paths,
         }),
-        previewer = conf.file_previewer({}),
+        -- previewer = conf.file_previewer({}),
         sorter = conf.generic_sorter({}),
     }):find()
 end
@@ -22,6 +22,8 @@ end
 vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
     { desc = "Open harpoon window" })
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>rr", function() harpoon:list():clear() end)
+vim.keymap.set("n", "<leader>rs", function() harpoon:list():remove() end)
 -- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 -- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
