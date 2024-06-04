310d43b36cc9cd01e2cc96ab9cfd3f928f91ca10
Tomas Dedic <tdedic@healthequity.com>
2024-06-04 07:40:15 +0200

    orphan apitest

--- .config/nvim/lua/pluginsconf/markdown.lua
+++ .config/nvim/lua/pluginsconf/markdown.lua
@@ -10,8 +10,8 @@ vim.g.vim_markdown_folding_level = 3
 --  vim.g.indentLine_concealcursor = "nv"
 -- vim.g.indentLine_concealcursor = "inc"
 -- vim.g.indentLine_char_list = "['|', '¦', '┆', '┊']"
-vim.g.vim_markdown_conceal_code_blocks = 0
+vim.g.vim_markdown_conceal_code_blocks = 1
 -- vim.g.vim_markdown_conceal = 2
-vim.cmd [[
+vim.cmd([[
 autocmd FileType markdown set ts=2 sts=2 sw=2 expandtab
-]]
+]])
