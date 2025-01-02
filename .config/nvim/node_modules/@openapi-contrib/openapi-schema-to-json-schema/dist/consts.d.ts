314f4ee123148a94e59da0c67af3431e90687905
Tomas Dedic <dedtom@gmail.com>
2025-01-02 16:29:02 +0100

    feat: yamlls

--- .config/nvim/node_modules/@openapi-contrib/openapi-schema-to-json-schema/dist/consts.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const NOT_SUPPORTED: readonly ["nullable", "discriminator", "readOnly", "writeOnly", "xml", "externalDocs", "example", "deprecated"];
-export declare const STRUCTS: readonly ["allOf", "anyOf", "oneOf", "not", "items", "additionalProperties"];
-export declare const VALID_OPENAPI_FORMATS: string[];
