01f4d28f4f15769184d2cd00bc1fcc821deb6d1d
Tomas Dedic <dedtom@gmail.com>
2025-02-25 18:57:03 +0100

    nvim mac

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -153,7 +153,7 @@ require("lazy").setup({
 				"hrsh7th/cmp-buffer",
 				"hrsh7th/cmp-path",
 				"hrsh7th/cmp-cmdline",
-				"chrisgrieser/cmp_yanky",
+				-- "chrisgrieser/cmp_yanky",
 				-- "lukas-reineke/cmp-rg",
 				-- { "hrsh7th/vim-vsnip",
 				-- config = function()
