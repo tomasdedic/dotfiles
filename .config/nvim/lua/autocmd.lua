3a5f323c895d57f3f71185a9c6e7902daf4c5c1b
Tomas Dedic <dedtom@gmail.com>
2025-06-02 12:13:40 +0200

    deploy: api + cm

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -261,8 +261,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	group = augroup("markdown_conceal"),
 	pattern = { "markdown" },
 	callback = function()
-		vim.opt_local.conceallevel = 0
-		vim.opt.foldmethod = "expr"
+		vim.opt_local.conceallevel = 1
 	end,
 })
 
