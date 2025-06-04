bafcbf8c581e815e1042fe4fa8e41f5acda1c469
Tomas Dedic <tdedic@healthequity.com>
2025-06-04 10:15:07 +0200

    deploy: mongodb allow meshpermissions

--- .config/nvim/lua/pluginsconf/lsp/lsp-lua.lua
+++ .config/nvim/lua/pluginsconf/lsp/lsp-lua.lua
@@ -1,26 +1,27 @@
 local M = {}
 
 M.setup = function()
-  local capabilities = require("pluginsconf.lsp.lsp-common").capabilities
-  local common_on_attach = require("pluginsconf.lsp.lsp-common").common_on_attach
+	local capabilities = require("pluginsconf.lsp.lsp-common").capabilities
+	local common_on_attach = require("pluginsconf.lsp.lsp-common").common_on_attach
 
-  local opts = {
-    on_attach = common_on_attach,
-    capabilities = capabilities,
-    settings = {
-      Lua = {
-        completion = {
-          callSnippet = "Replace",
-        },
-        diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      },
-    },
-  }
+	local opts = {
+		on_attach = common_on_attach,
+		capabilities = capabilities,
+		settings = {
+			Lua = {
+				completion = {
+					callSnippet = "Replace",
+				},
+				diagnostics = {
+					-- Get the language server to recognize the `vim` global
+					globals = { "vim" },
+				},
+			},
+		},
+	}
 
-  require("lspconfig").lua_ls.setup(vim.tbl_deep_extend("force", opts, {}))
+	-- require("lspconfig").lua_ls.setup(vim.tbl_deep_extend("force", opts, {}))
+	require("lspconfig").lua_ls.setup(opts)
 end
 
 return M
