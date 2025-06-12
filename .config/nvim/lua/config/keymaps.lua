5cd2852a9bb4de5a3346f91c068a8e2fae13fd92
Tomas Dedic <tdedic@healthequity.com>
2025-06-12 15:55:39 +0200

    deploy: head

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,10 +7,36 @@ vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", {})
 vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
 vim.keymap.set("n", "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<CR>", {})
 vim.keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNext<CR>", {})
-vim.keymap.set("n", ";", "<cmd>FzfLua buffers sort_mru=true previewer=false sort_lastused=true<cr>", {})
+vim.keymap.set(
+  "n",
+  ";",
+  "<cmd>FzfLua buffers sort_mru=true previewer=false sort_lastused=true<cr>",
+  { desc = "Bufers noPreview" }
+)
 vim.keymap.set("n", "-", function()
   Snacks.explorer({ cwd = LazyVim.root() })
 end, {})
 -- vim.keymap.set("n", "<leader>cd", "<cmd>lcd %:p:h<CR>", {})
 --
 vim.keymap.set("n", "<leader>dd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+vim.api.nvim_create_user_command("Dir", function()
+  vim.fn.setreg("+", vim.fn.expand("%:h:"))
+end, { nargs = 0 })
+vim.api.nvim_create_user_command("File", function()
+  vim.fn.setreg("+", vim.fn.expand("%:p:."))
+end, { nargs = 0 })
+
+vim.api.nvim_create_user_command("Name", function()
+  vim.fn.setreg("+", vim.fn.expand("%:t:r"))
+end, { nargs = 0 })
+-- <leader>fl
+-- { "<leader>fF", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+vim.keymap.set(
+  "n",
+  "<leader>fl",
+  LazyVim.pick("files", { cwd = vim.fn.expand("%:h:") }),
+  { desc = "Pick files under current buffer" }
+)
+vim.keymap.set("n", "<leader>sB", function()
+  Snacks.picker.grep_buffers()
+end, { desc = "Grep Open Buffers" })
