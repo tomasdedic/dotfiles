fa475c7b4fbd119e24fe316662f56e72387fa98a
Tomas Dedic <dedtom@gmail.com>
2025-06-25 22:39:05 +0200

    deploy: head

---
+++ .config/nvim/lua/plugins/yaml.lua
@@ -0,0 +1,61 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        yamlls = {
+          -- Have to add this for yamlls to understand that we support line folding
+          capabilities = {
+            textDocument = {
+              foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true,
+              },
+            },
+          },
+          -- lazy-load schemastore when needed
+          on_new_config = function(new_config)
+            new_config.settings.yaml.schemas = vim.tbl_deep_extend(
+              "force",
+              new_config.settings.yaml.schemas or {},
+              require("schemastore").yaml.schemas({
+                select = {
+                  "kustomization.yaml",
+                  "GitHub Workflow",
+                  "gRPC API Gateway & OpenAPI Config",
+                  -- "Kong DBLess config",
+                },
+                -- replace = {
+                --   ["Kong DBLess config"] = {
+                --     name = "Kong DBLess config",
+                --     fileMatch = { "**/api-docs/**/*.yaml" },
+                --     url = "https://json.schemastore.org/kong_json_schema.json",
+                --     description = "patch kong",
+                --   },
+                -- },
+              })
+            )
+          end,
+          settings = {
+            redhat = { telemetry = { enabled = false } },
+            yaml = {
+              keyOrdering = false,
+              format = {
+                enable = true,
+              },
+              validate = true,
+              schemaStore = {
+                -- Must disable built-in schemaStore support to use
+                -- schemas from SchemaStore.nvim plugin
+                enable = false,
+                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                url = "",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
