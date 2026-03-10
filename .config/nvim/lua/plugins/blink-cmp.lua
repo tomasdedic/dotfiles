6fe9f54212f9e216acd5f55b534d96e38a47b0f9
Tomas Dedic <dedtom@gmail.com>
2026-03-10 10:12:44 +0100

    agent tmux split, max recurse 1 for tmux

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -1,3 +1,17 @@
+--  Key                        Action
+--  <C-space>  show, show_documentation, hide_documentation
+--  <C-e>      hide, fallback
+--  <C-y>      select_and_accept
+--  <C-p>      select_prev
+--  <C-n>      select_next
+--  <Up>       select_prev, fallback
+--  <Down>     select_next, fallback
+--  <C-b>      scroll_documentation_up
+--  <C-f>      scroll_documentation_down
+--  <Tab>      snippet_forward, fallback
+--  <S-Tab>    snippet_backward, fallback
+--  <C-k>      show_signature, hide_signature, fallback
+
 --add completion from all buffers
 return {
   {
